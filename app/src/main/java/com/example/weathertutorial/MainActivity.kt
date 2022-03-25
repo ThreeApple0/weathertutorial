29a24931a983692b4b8a186c4c3f7babb4ea889a
Three_Apple <73579842+ThreeApple0@users.noreply.github.com>
2022-03-25 16:23:54 +0900

    220325

--- app/src/main/java/com/example/weathertutorial/MainActivity.kt
+++ app/src/main/java/com/example/weathertutorial/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.weathertutorial
 
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ProgressBar
+import android.widget.RelativeLayout
 
 class MainActivity : AppCompatActivity() {
+    val CITY: String = "dhaka,bd"
+    val API: String = "0e863d0ed1d76744cb0ad1fb01a10d3c"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        weatherTask().execute()
+    }
+
+    inner class weatherTask(): AsyncTask<String, Void,String>{
+        override fun onPreExecute() {
+            super.onPreExecute()
+            findViewById<ProgressBar>(R.id.loader).visibility = View.VISIBLE
+            findViewById<RelativeLayout>(R.id.)
+        }
     }
 }
