bdc90674e466d1d79f9469abe557e0d15278092a
Three_Apple <73579842+ThreeApple0@users.noreply.github.com>
2022-04-08 15:41:52 +0900

    220408

--- app/src/main/java/com/example/weathertutorial/MainActivity.kt
+++ app/src/main/java/com/example/weathertutorial/MainActivity.kt
@@ -6,6 +6,9 @@ import android.os.Bundle
 import android.view.View
 import android.widget.ProgressBar
 import android.widget.RelativeLayout
+import android.widget.TextView
+import org.json.JSONObject
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
     val CITY: String = "dhaka,bd"
@@ -17,11 +20,40 @@ class MainActivity : AppCompatActivity() {
         weatherTask().execute()
     }
 
-    inner class weatherTask(): AsyncTask<String, Void,String>{
+    inner class weatherTask(): AsyncTask<String, Void,String>(){
         override fun onPreExecute() {
             super.onPreExecute()
             findViewById<ProgressBar>(R.id.loader).visibility = View.VISIBLE
-            findViewById<RelativeLayout>(R.id.)
+            findViewById<RelativeLayout>(R.id.main_C).visibility = View.GONE
+            findViewById<TextView>(R.id.errortext).visibility = View.GONE
+        }
+
+        override fun doInBackground(vararg p0: String?): String? {
+            var response:String?
+            try{
+                response = URL("https://api.openweathermap.org/data/2.5/weather?q=$CITY&APPID=$API")
+                    .readText(Charsets.UTF_8)
+            }
+            catch (e:Exception){
+                response=null
+            }
+
+            return response
+        }
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+            try {
+                val jsonobj = JSONObject(result)
+                val main = jsonobj.getJSONObject("main")
+                val sys = jsonobj.getJSONObject("sys")
+                val wind = jsonobj.getJSONObject("wind")
+                val weather = jsonobj.getJSONArray("weather").getJSONObject(0)
+                val updatedAt:Long = jsonobj.getLong("dt")
+            }
+            catch (e:Exception){
+
+            }
         }
     }
 }
